ef3ec262531f7358c4b6d673229736a785d8ecaa
Bret Gourdie <BretGourdie@gmail.com>
2017-02-19 01:10:34 -0500

    Created basic methods
    Untested because Visual Studio Code compilation isn't working

--- Data Structures/Vector.cs
+++ Data Structures/Vector.cs
@@ -6,7 +6,89 @@ using System.Threading.Tasks;
 
 namespace Data_Structures
 {
-    class Vector
+    public class Vector<T>
     {
+        private T[] _array;
+        private int _size;
+
+        public Vector(int capacity)
+        {
+            _array = new T[capacity];
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _array[index];
+        }
+
+        public bool Add(T item)
+        {
+            if(_size < _capacity)
+            {
+                _array = resizeArray(_capacity * 2);
+            }
+
+            _array[_size] = item;
+
+            return true;
+        }
+
+        public bool Insert(int index, T item)
+        {
+            if(index >= _size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if(index >= _capacity)
+            {
+                _array = resizeArray(_capacity * 2);
+            }
+
+            _array[index] = item;
+
+            return true;
+        }
+
+        public T Delete(int index, T item)
+        {
+            T itemToReturn = null;
+
+            if(index >= _size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            itemToReturn = _array[index];
+
+            for(int ii = index; ii < _size; ii++)
+            {
+                if(ii+1 < _size)
+                {
+                    _array[ii] = _array[ii+1];
+                }
+            }
+
+            _array[_size] = null;
+
+            return itemToReturn;
+        }
+
+        private T[] resizeArray(int newSize)
+        {
+            var newArray = new T[newSize];
+
+            for(int ii = 0; ii < _array.Length; ii++)
+            {
+                newArray[ii] = _array[ii]
+            }
+
+            return newArray;
+        }
     }
 }
