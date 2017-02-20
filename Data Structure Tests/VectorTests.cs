8283f1469f4f9ea5b7a3762238a5c1b046558966
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2017-02-19 22:27:38 -0500

    Initial add of VectorTests

---
+++ Data Structure Tests/VectorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data_Structure_Tests
+{
+    [TestClass]
+    class VectorTests
+    {
+
+        [TestMethod]
+        public void EmptyVector()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
